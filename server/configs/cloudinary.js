a3238b2c186e2ca76ea76ec6a74689b8ffcbcbbb
Tushar Tyagi <tushartyagi389@gmail.com>
2025-07-11 23:25:46 +0530

    cloudinary changed

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,7 +1,7 @@
 // server/configs/cloudinary.js
 import { v2 as cloudinary } from "cloudinary";
 
-// Directly configure Cloudinary on import
+// Configure Cloudinary immediately on import
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
